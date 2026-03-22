9135d4b4c3015131964ee8548317b2f73cfd4244
RandomCrocodile <mawi@polosab.com>
2026-03-22 19:28:13 +0100

    --show-all fix with --improved-forms

--- Extensions/ILSpy.Decompiler/dnSpy.Decompiler.ILSpy.Core/CSharp/CSharpDecompiler.cs
+++ Extensions/ILSpy.Decompiler/dnSpy.Decompiler.ILSpy.Core/CSharp/CSharpDecompiler.cs
@@ -516,7 +516,7 @@ namespace dnSpy.Decompiler.ILSpy.Core.CSharp {
 			newOne.UsingDeclarations = useUsingDeclarations;
 			newOne.FullyQualifyAllTypes = !useUsingDeclarations;
 			newOne.RemoveNewDelegateClass = useUsingDeclarations;
-			newOne.ForceShowAllMembers = false;
+			newOne.ForceShowAllMembers = settings.ForceShowAllMembers;
 			return newOne;
 		}
 
